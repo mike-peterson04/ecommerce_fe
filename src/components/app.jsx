8abdaa4ec744a801f7dcfac3d7f5e6b7b6539d58
Nessiah919 <nesmith.charlesii@gmail.com>
2021-06-16 14:16:40 -0400

    created 'App' component and 'app.css'. imported app.css to App component

---
+++ src/components/app.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './app.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="container-fluid">
+                <h1>Test</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
