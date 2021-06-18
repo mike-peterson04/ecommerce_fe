e79f179ff521b988e2385dbfd5ea69d369e9aeeb
njdsm <nathanial.r.johnson@gmail.com>
2021-06-18 10:09:27 -0500

    shopping cart now successfully renders product id. Working on how to style it out

--- src/components/ShoppingCart/shoppingCart.jsx
+++ src/components/ShoppingCart/shoppingCart.jsx
@@ -5,11 +5,14 @@ import './shoppingCart.css';
 
 const ShoppingCart = (props) => {
     
-    console.log(props.user);
-
     return (
         <div>
             <h1>{props.user.username}'s Shopping Cart.</h1>
+            {props.items.map((item) => 
+                <div>
+                    <p>ID: {item.productId}</p>
+                </div>
+            )}
         </div>
     )
 }
